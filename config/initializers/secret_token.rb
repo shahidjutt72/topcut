6d4cbdfeae0ad8345aef1f4cb270a895b77d10bf
Shahid <shahidjutt72@yahoo.com>
2014-03-22 00:18:08 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Topcut::Application.config.secret_key_base = '506594e7207a2fcb52562839d4c2ea94130e100ec32e571faacc1e60359db9ea2ccb7973fc71337737ad8cb202df62995dca5e1e0902b3b3082e8aa1aeac14f9'
