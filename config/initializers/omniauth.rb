98f69160f5056728b99308342ef9d76f84f1c7da
Shahid <shahidjutt72@yahoo.com>
2014-04-06 07:09:28 -0700

    fixed issue in facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do	
-	provider :facebook, '1571098119782327', 'a8f11136f246cb127661a1dad243433c'
+	provider :facebook, '592469394125210', 'ac4e7a42aea973b5129dc4ffec77dbfe'
 end
