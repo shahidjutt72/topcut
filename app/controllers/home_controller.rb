437e99ee164939941934d6004bb3745f9e694760
Shahid <shahidjutt72@yahoo.com>
2014-06-21 12:20:25 -0700

    added the main page popup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
 	def index
-		@slot = Slot.new
-		@company = current_user.company
-		@services = @company.services
-		@staffs = @company.staffs		
+		if current_user and current_user.company
+			@slot = Slot.new
+			@company = current_user.company
+			@services = @company.services
+			@staffs = @company.staffs
+		end			
 	end	
 end
