6845efae2ec0f21ddf13fdf35f77ed8fd9eecabd
Shahid <shahidjutt72@yahoo.com>
2014-06-27 12:22:13 -0700

    addedd all the chan ges

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
 			@slot = Slot.new			
 			@services = @company.services
 			@staffs = @company.staffs
+			@slots = @company.slots
+			if params[:staff] !=  nil and params[:staff] !=  ""
+				@slots = @company.slots.where("staff_id =?",params[:staff].to_i)
+			end
 		end			
 	end	
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ActiveRecord::Base
-	has_many :slots
+	has_many :slots, :dependent => :destroy
 	belongs_to :company
 	def fulladdress
 		"#{self.address},#{self.city},#{self.ountry}"
