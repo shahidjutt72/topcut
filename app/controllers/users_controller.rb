3950bfd0df4487e4e6de51bad3bbbca9d22f1c49
shahid <shahidjutt72@yahoo.com>
2014-07-17 09:06:28 -0700

    all changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
 	def get_calendar_updates
 		service = Service.find_by_id(params[:service_id])
+		@company = Company.find_by_id(params[:company_id])
 		date = params[:date].to_date
 		render :partial =>"users/slot_timings", :locals =>{:date => date, :service =>service}
 	end	
