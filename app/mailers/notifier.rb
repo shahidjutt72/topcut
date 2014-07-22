8536cbfc79d6f5b3cca43a4c243bb98f02162e2d
shahid <shahidjutt72@yahoo.com>
2014-07-22 22:58:08 +0500

    added the email changes

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,10 +6,15 @@ class Notifier < ActionMailer::Base
   #
   #   en.notifier.customer_email.subject
   #
-  def customer_email(staff,slot,customer,service)
-    @greeting = "Hi"    
+  def customer_email(staff,slot,customer,service,company)
+    @staff = staff
+    @slot = slot
+    @customer = customer
+    @service = service
+    @company = company
+    @time = "#{slot.slot_start_time.strftime("%A")}, #{slot.slot_start_time.strftime("%d")} #{slot.slot_start_time.strftime("%b")} #{slot.slot_start_time.strftime("%Y")} at #{slot.slot_start_time.strftime("%I:%M %p")}"    
 
-    mail to: staff.email, :subject =>"Appointment Scheduled for Thursday, 24 Jul 2014 at 12:10 pm with shahid customer."
+    mail to: customer.email, :subject =>"Appointment Scheduled for #{@time} with #{staff.name}."
   end
 
   def staff_email(staff,slot,customer,service,company)
